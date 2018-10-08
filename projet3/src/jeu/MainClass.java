000fe8a3dd35b244f37297198d84a93b28952a97
Herbert Caffarel <herbert.caffarel@cicef.pro>
2018-10-08 19:54:27 +0200

    Relecture code simplifié du main

---
+++ projet3/src/jeu/MainClass.java
@@ -0,0 +1,132 @@
+package jeu;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class MainClass {
+
+	@SuppressWarnings("resource")
+	public static void main(String[] args) {
+
+		int etendue = 9;
+		int taille = 4;
+		int gameTypeChoice = 2;
+		int attackDefenseChoice;
+
+		Jeu jeu = null;
+
+		Scanner scanner = new Scanner(System.in);
+
+		// Saisie des parametres de configuration
+		etendue = askIntegerValue(1, 9, "Entrer l'étendue");
+		taille = askIntegerValue(1, 6, "Entrer la taille");
+		gameTypeChoice = askIntegerValue(1, 2, "Quel jeu voulez vous jouer?\n1: Mastermind , 2: PlusouMoins");
+		attackDefenseChoice = askIntegerValue(1, 2, "Ordinateur attaquant (1) ou défenseur (2) ?");
+
+		// Creation du jeu
+		switch (gameTypeChoice) {
+		case 1:
+			jeu = new Mastermind();
+			break;
+
+		case 2:
+			jeu = new PlusouMoins(etendue, taille);
+			break;
+		}
+
+		// Resolution du jeu
+		switch (attackDefenseChoice) {
+		case 1: // Ordinateur attaque
+			System.out.println(boucleJeuAttaquant(jeu, gameTypeChoice, etendue, taille));
+			break;
+		case 2: // ordinateur d�fend
+			System.out.println(boucleJeuDefenseur(jeu, gameTypeChoice, etendue, taille));
+			break;
+		default:
+			System.out.println("Choix de jeu invalide");
+		}
+	}
+
+	// Méthode d'aide à la saisie d'un nombre entre min et max inclus
+	private static int askIntegerValue(int min, int max, String msg) {
+		int value = 0;
+		boolean valide = false;
+		Scanner scanner = new Scanner(System.in);
+
+		while (!valide) {
+			try {
+				System.out.println(msg);
+				value = scanner.nextInt();
+				if ((value >= min) && (value <= max)) {
+					valide = true;
+				} else {
+					System.out.println("value incorrecte (entre " + min + " et " + max + ")");
+				}
+			} catch (InputMismatchException ime3) {
+				System.out.println("Ce n'est pas un entier !");
+				valide = false;
+			} finally {
+				scanner.nextLine();
+			}
+
+		}
+		return value;
+	}
+
+	private static String boucleJeuAttaquant(Jeu jeu, int choice, int etendue, int taille) {
+		int NombreEssaiMax = 10;
+		boolean trouve = false;
+		String message;
+		Scanner scanner = new Scanner(System.in);
+		for (int i = 0; i < NombreEssaiMax && !trouve; i++) {
+			System.out.print("Le code proposé par l'ordinateur : ");
+			System.out.println(jeu.chercherSolution());
+			System.out.println("Quel est le resultat pour cette proposition ?");
+			boolean resultatValide = false;
+			while (!resultatValide) {
+				String resultat = scanner.nextLine();
+				try {
+					for (char c : resultat.toCharArray()) {
+						if (!((c == '=') || (c == '-') || (c == '+'))) {
+							throw new InputMismatchException();
+						}
+					}
+					jeu.analyserResultat(resultat);
+					resultatValide = true;
+					trouve = jeu.isWon(resultat);
+				} catch (InputMismatchException ime) {
+					System.out.println("Vous vous êtes trompé, veuillez ressaisir le résultat");
+				}
+			}
+		}
+		message = ("Vous avez " + ((trouve) ? "gagné" : "perdu"));
+		return message;
+	}
+
+	private static String boucleJeuDefenseur(Jeu jeu, int choice, int etendue, int taille) {
+		int NombreEssaiMax = 10;
+		boolean trouve = false;
+		String message;
+		Scanner scanner = new Scanner(System.in);
+		jeu.setCodeATrouverParLeJoueur(Code.genererNewCode(etendue, taille));
+
+		// Boucle de recherche de la solution par le joueur
+		for (int i = 0; i < NombreEssaiMax && !trouve; i++) {
+			// On affiche le code à trouver
+			System.out.println("Voici le code à deviner");
+			System.out.println(jeu.getCodeATrouverParLeJoueur());
+
+			if (!trouve) {
+				System.out.println("Entrer un nouveau code?");
+				jeu.enterCode();
+
+				System.out.println("Le résultat est : " + jeu.comparerCode());
+				trouve = jeu.isWon(jeu.getResultatPourLeJoueur());
+			}
+
+		}
+		message = ("Vous avez " + ((trouve) ? "gagné" : "perdu"));
+		return message;
+
+	}
+}
