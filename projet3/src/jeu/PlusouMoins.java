cf49271827812c91d85ee602eb4a222d1c9c668b
victorle876 <victorl648@gmail.com>
2018-11-25 21:20:14 +0100

    ajout javadoc pour la classe plusoumoins

--- projet3/src/jeu/PlusouMoins.java
+++ projet3/src/jeu/PlusouMoins.java
@@ -6,7 +6,12 @@ public class PlusouMoins extends Jeu {
 	private ArrayList<Integer> min = new ArrayList<>();
 	private ArrayList<Integer> max = new ArrayList<>();
 
-	// Constructeur
+	/**
+	 * Méthode permettant d'ajouter 0 à l'ArrayList Min
+	 * et etendue ArrayList Max
+	 * @param etendue
+	 * @param taille
+	 */
 	public PlusouMoins(int etendue, int taille) {
 		this.etendue = etendue;
 		this.taille = taille;
@@ -16,7 +21,12 @@ public class PlusouMoins extends Jeu {
 			max.add(etendue);
 		}
 	}
-
+	
+    /**
+     * Méthode permettant de comparer le code à trouver avec celui des différntes
+     * propositions de l'ordinateur
+     * Et ensuite, de donner le résultat à l'ordinateur
+     */
 	@Override
 	public String comparerCode() {
 		resultatPourLeJoueur = "";
@@ -43,12 +53,14 @@ public class PlusouMoins extends Jeu {
 		return (resultat.matches("^=+$"));
 	}
 
+	
+	/**
+	 * Pour chaque chiffre du code, je propose la moitié entre le min et le max de
+	 * ce chiffre Je construit donc un code proposé à partir de ces valeurs
+	 */
 	@Override
 	public Code chercherSolution() {
-		/*
-		 * Pour chaque chiffre du code, je propose la moitié entre le min et le max de
-		 * ce chiffre Je construit donc un code proposé à partir de ces valeurs
-		 */
+		
 		Integer chiffre;
 		this.propositionOrdinateur.clear();
 		for (int i = 0; i < this.taille; i++) {
@@ -60,16 +72,18 @@ public class PlusouMoins extends Jeu {
 	public void demandeAnalyse() {
 		//todo
 	}
+	
+	/**
+	 * pour chaque chiffre du code, selon la valeur du résultat correspondant, je
+	 * repositionne mon min ou mon max si le chiffre est trop grand, le max devient
+	 * ce chiffre si le chiffre est trop petit, le min devient ce chiffre si le
+	 * chiffre est le bon, min et max deviennent ce chiffre
+	 * 
+	 */
 
 	@Override
 	public void analyserResultat(String resultat) {
-		/*
-		 * pour chaque chiffre du code, selon la valeur du résultat correspondant, je
-		 * repositionne mon min ou mon max si le chiffre est trop grand, le max devient
-		 * ce chiffre si le chiffre est trop petit, le min devient ce chiffre si le
-		 * chiffre est le bon, min et max deviennent ce chiffre
-		 * 
-		 */
+	
 
 		for (int i = 0; i < resultat.length(); i++) {
 			if (resultat.charAt(i) == '-') {
